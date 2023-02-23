5e49144a74c453c22342ad6c9893a834e954f536
HazemSabry <89419013+HazemSabry@users.noreply.github.com>
2023-02-23 22:14:21 +0200

    Update Education.jsx

--- src/scenes/Education.jsx
+++ src/scenes/Education.jsx
@@ -3,7 +3,6 @@ import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 
 const MySkills = () => {
-  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
   return (
     <section id="skills" className="pt-10 pb-24">
       {/* HEADER AND IMAGE SECTION */}
