603e52018ec275e8458b400d3a462b6ca55877f8
HazemSabry <89419013+HazemSabry@users.noreply.github.com>
2023-02-24 15:40:37 +0200

    Update Navbar.jsx

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
               setSelectedPage={setSelectedPage}
             />
             <Link
-              page="Certification"
+              page="Education"
               selectedPage={selectedPage}
               setSelectedPage={setSelectedPage}
             />
@@ -93,7 +93,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
                 setSelectedPage={setSelectedPage}
               />
               <Link
-                page="Certification"
+                page="Education"
                 selectedPage={selectedPage}
                 setSelectedPage={setSelectedPage}
               />
