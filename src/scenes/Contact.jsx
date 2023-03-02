8097cc69cd76e9c88fc289853f3e7953f4a08a3a
HazemSabry <89419013+HazemSabry@users.noreply.github.com>
2023-03-02 22:17:36 +0200

    Update Contact.jsx

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -59,7 +59,7 @@ const Contact = () => {
           }}
           className="basis-1/2 mt-10 md:mt-0"
         >
-          <form name="contact" netlify>
+          <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
             <label>
               <input
                 className="w-full bg-blue font-semibold placeholder-opaque-black p-3"
